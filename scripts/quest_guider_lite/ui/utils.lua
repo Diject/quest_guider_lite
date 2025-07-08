02c4f16c196711a921c2e59862a1f3b7eb1bdb9d
Diject <alexkozlovv@gmail.com>
2025-07-08 12:30:19 +0300

    fix text height calculation to handle newlines

--- scripts/quest_guider_lite/ui/utils.lua
+++ scripts/quest_guider_lite/ui/utils.lua
@@ -8,17 +8,21 @@ function this.getTextHeight(text, fontSize, width, mul)
     local charWidth = fontSize * mul
     local rowMaxSize = math.floor(width / charWidth)
     local rowCount = 1
-    local currentRowSize = 0
-    for word in text:gmatch("%S+") do
-        local wordSize = utf8.len(word)
-        if currentRowSize + wordSize <= rowMaxSize then
-            currentRowSize = currentRowSize + wordSize + 1
-        else
-            rowCount = rowCount + 1
-            currentRowSize = wordSize
+    for line in text:gmatch("[^\n]+") do
+        local currentRowSize = 0
+        for word in line:gmatch("%S+") do
+            local wordSize = utf8.len(word)
+            if currentRowSize + wordSize <= rowMaxSize then
+                currentRowSize = currentRowSize + wordSize + 1
+            else
+                rowCount = rowCount + 1
+                currentRowSize = wordSize
+            end
+            table.insert(words, word)
         end
-        table.insert(words, word)
+        rowCount = rowCount + 1
     end
+    rowCount = rowCount - 1
     return rowCount * fontSize, rowCount
 end
 
