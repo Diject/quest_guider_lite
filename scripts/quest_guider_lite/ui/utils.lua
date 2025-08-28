9482c31f98f7d7fc0219654a1400ff83eefe8051
Diject <alexkozlovv@gmail.com>
2025-08-28 19:50:55 +0300

    add alternative colorize function

--- scripts/quest_guider_lite/ui/utils.lua
+++ scripts/quest_guider_lite/ui/utils.lua
@@ -84,6 +84,53 @@ function this.colorize(text, search, color, defaultColor)
 end
 
 
+function this.colorizeNested(text, search, color, defaultColor)
+    if not search or #search == 0 then return text end
+
+    color = color or "#000000"
+    defaultColor = defaultColor or ("#"..config.data.ui.defaultColor:asHex())
+
+    local pattern = ""
+    for i = 1, #search do
+        local c = search:sub(i, i)
+        if c:match("%a") then
+            pattern = pattern .. "[" .. c:lower() .. c:upper() .. "]"
+        elseif specialCharacters[c] then
+            pattern = pattern .. "%" .. c
+        else
+            pattern = pattern .. c
+        end
+    end
+
+    local result = ""
+    local lastIndex = 1
+    local currentColor = defaultColor
+
+    for i, c in text:gmatch("()(#%x%x%x%x%x%x)") do
+        local segment = text:sub(lastIndex, i - 1)
+        if #segment > 0 then
+            local replacedSegment = segment:gsub(pattern, function(found)
+                return color .. found .. currentColor
+            end)
+            result = result .. replacedSegment
+        end
+        result = result .. c
+        currentColor = c
+        lastIndex = i + #c
+    end
+
+    local finalSegment = text:sub(lastIndex)
+    if #finalSegment > 0 then
+        local replacedSegment = finalSegment:gsub(pattern, function(found)
+            return color .. found .. currentColor
+        end)
+        result = result .. replacedSegment
+    end
+
+    return result
+end
+
+
 function this.removeColorMarkers(text)
     return text:gsub("#%x%x%x%x%x%x", "")
 end
