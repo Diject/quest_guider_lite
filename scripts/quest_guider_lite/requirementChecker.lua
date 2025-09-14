b37a3f6ee2a4943114b21b4f074f396cc8d5ed0a
Diject <alexkozlovv@gmail.com>
2025-09-14 19:11:52 +0300

    add support for additional requirement types to checker

--- scripts/quest_guider_lite/requirementChecker.lua
+++ scripts/quest_guider_lite/requirementChecker.lua
@@ -1,21 +1,66 @@
 local include = require("scripts.quest_guider_lite.utils.include")
 
 local types = require('openmw.types')
+local core = require("openmw.core")
 local playerFunc = types.Player
 local world = include("openmw.world")
-local playerRef = include("openmw.self")
+local tes3 = include("scripts.quest_guider_lite.core.tes3")
+local playerRef = include("openmw.self") or world.players[1] ---@diagnostic disable-line: need-check-nil
 
 local stringLib = require("scripts.quest_guider_lite.utils.string")
 local tableLib = require("scripts.quest_guider_lite.utils.table")
 local reqTypes = require("scripts.quest_guider_lite.types")
 local operator = reqTypes.operator
 local killCounter = require("scripts.quest_guider_lite.killCounter")
+local log = require("scripts.quest_guider_lite.utils.log")
 
 local playerQuests = require("scripts.quest_guider_lite.playerQuests")
 
 local getObject = require("scripts.quest_guider_lite.core.getObject")
 
 local this = {}
+
+
+local skillFuncs = {
+    types.NPC.stats.skills.block,
+    types.NPC.stats.skills.armorer,
+    types.NPC.stats.skills.mediumarmor,
+    types.NPC.stats.skills.heavyarmor,
+    types.NPC.stats.skills.bluntweapon,
+    types.NPC.stats.skills.longblade,
+    types.NPC.stats.skills.axe,
+    types.NPC.stats.skills.spear,
+    types.NPC.stats.skills.athletics,
+    types.NPC.stats.skills.enchant,
+    types.NPC.stats.skills.destruction,
+    types.NPC.stats.skills.alteration,
+    types.NPC.stats.skills.illusion,
+    types.NPC.stats.skills.conjuration,
+    types.NPC.stats.skills.mysticism,
+    types.NPC.stats.skills.restoration,
+    types.NPC.stats.skills.alchemy,
+    types.NPC.stats.skills.unarmored,
+    types.NPC.stats.skills.security,
+    types.NPC.stats.skills.sneak,
+    types.NPC.stats.skills.acrobatics,
+    types.NPC.stats.skills.lightarmor,
+    types.NPC.stats.skills.shortblade,
+    types.NPC.stats.skills.marksman,
+    types.NPC.stats.skills.merchantile,
+    types.NPC.stats.skills.speechcraft,
+    types.NPC.stats.skills.handtohand,
+}
+
+local attributeFuncs = {
+    types.Actor.stats.attributes.strength,
+    types.Actor.stats.attributes.intelligence,
+    types.Actor.stats.attributes.willpower,
+    types.Actor.stats.attributes.agility,
+    types.Actor.stats.attributes.speed,
+    types.Actor.stats.attributes.endurance,
+    types.Actor.stats.attributes.personality,
+    types.Actor.stats.attributes.luck,
+}
 
 
 ---@type table<string, fun(req:questDataGenerator.requirementData, obj:any, mobile:any, ref:tes3reference):boolean?>
@@ -133,6 +178,360 @@ local dataFuncs = {
         return true
     end,
 
+    [reqTypes.requirementType.NotActorID] = function (req, ref)
+        if not req.variable or not ref then return end
+        return not operator.check(req.variable, ref.recordId, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerExpelledFromNPCFaction] = function (req, ref)
+        if not req.value or not ref then return end
+
+        local factions = types.NPC.getFactions(ref)
+        if not factions then return false end
+        local res = 0
+
+        for _, faction in pairs(factions) do
+            res = types.NPC.isExpelled(playerRef, faction) and 1 or 0
+            if res == 1 then break end
+        end
+
+        return operator.check(res, ref.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCSameFactionAsPlayer] = function (req, ref)
+        if not req.value or not ref then return end
+
+        local factions = types.NPC.getFactions(ref)
+        if not factions then return false end
+        local res = 0
+        for _, faction in pairs(factions) do
+            res = types.NPC.getFactionRank(playerRef, faction) and 1 or 0
+            if res == 1 then break end
+        end
+
+        return operator.check(res, ref.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.ValueFLTV] = function (req, ref)
+        if not req.variable or not req.value or not ref or not world then return end
+
+        local script = world.mwscript.getLocalScript(ref, playerRef)
+        if not script then return  end
+
+        local value
+        for name, val in pairs(script.variables) do
+            if name:lower() == req.variable then
+                value = val
+                break
+            end
+        end
+
+        if not value then return false end
+
+        return operator.check(value, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NotActorClass] = function (req, ref)
+        if not req.variable or not req.value or not ref then return end
+        if not types.NPC.objectIsInstance(ref) then return end
+
+        local record = types.NPC.record(ref)
+        if not record then return end
+
+        local val = record.class:lower() ~= req.variable and 1 or 0
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NotActorCell] = function (req, ref)
+        if not req.variable or not req.value or not ref then return end
+
+        local val = string.sub(ref.cell.name, 1, #req.value):lower() ~= req.value and 1 or 0
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.CustomActorCell] = function (req, ref)
+        if not req.value or not ref then return end
+
+        return string.sub(ref.cell.name, 1, #req.value):lower() == req.value and true or false
+    end,
+
+    [reqTypes.requirementType.CustomPCCell] = function (req)
+        if not req.value or not req.variable then return end
+
+        local val = string.sub(playerRef.cell.name, 1, #req.variable):lower() == req.variable and 1 or 0
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCHealthPercent] = function (req, ref)
+        if not req.value or not ref then return end
+
+        local health = types.Actor.stats.dynamic.health(ref)
+        local val = health.current / health.base * 100
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerRankMinusNPCRank] = function (req, ref)
+        if not req.value or not ref then return end
+
+        local val = 0
+        local factions = types.NPC.getFactions(ref)
+        if factions then
+            for _, faction in pairs(factions) do
+                local plRank = types.NPC.getFactionRank(ref, faction)
+                local rank = types.NPC.getFactionRank(ref, faction)
+                return operator.check(plRank - rank, req.value, req.operator)
+            end
+        end
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NotActorRace] = function (req, ref)
+        if not req.variable or not req.value or not ref then return end
+        if not types.NPC.objectIsInstance(ref) then return end
+
+        local record = types.NPC.record(ref)
+        if not record then return end
+
+        local val = record.race:lower() ~= req.variable and 1 or 0
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerHealthPercent] = function (req)
+        if not req.value then return end
+
+        local health = types.Actor.stats.dynamic.health(playerRef)
+        local val = health.current / health.base * 100
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerIsVampire] = function (req)
+        if not req.value or not world then return end
+        local globals = world.mwscript.getGlobalVariables(playerRef)
+        if not globals then return end
+
+        local value = 0
+        for name, val in pairs(globals) do
+            if name:lower() == "pcvampire" then
+                value = val
+                break
+            end
+        end
+
+        return operator.check(value, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerCrimeLevel] = function (req)
+        if not req.value then return end
+        return operator.check(types.Player.getCrimeLevel(playerRef), req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCSameGenderAsPlayer] = function (req, ref)
+        if not req.value or not ref then return end
+        if not types.NPC.objectIsInstance(ref) then return end
+
+        local record = types.NPC.record(ref)
+        if not record then return end
+        local playerRecord = types.NPC.record(playerRef)
+
+        local val = record.isMale == playerRecord.isMale and 1 or 0
+
+        return operator.check(val, ref.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.CustomSkill] = function (req, ref)
+        if not req.value or not req.skill then return end
+        if req.object == "player" then
+            ref = playerRef
+        end
+        if not types.NPC.objectIsInstance(ref) then return end
+
+        local skillFunc = skillFuncs[req.skill]
+        if not skillFunc then return end
+
+        return operator.check(skillFunc(ref).modified, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.CustomAttribute] = function (req, ref)
+        if not req.value or not req.attribute then return end
+        if req.object == "player" then
+            ref = playerRef
+        end
+
+        local attrFunc = attributeFuncs[req.attribute]
+        if not attrFunc then return end
+
+        return operator.check(attrFunc(ref).modified, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerClothingModifier] = function (req)
+        if not req.value then return end
+
+        local val = 0
+        for slot, item in pairs(types.Actor.getEquipment(playerRef)) do
+            val = val + (item.type.record(item.recordId).value or 0)
+        end
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCSameRaceAsPlayer] = function (req, ref)
+        if not req.value or not ref then return end
+        if not types.NPC.objectIsInstance(ref) then return end
+
+        local record = types.NPC.record(ref)
+        if not record then return end
+        local playerRecord = types.NPC.record(playerRef)
+
+        local val = record.race == playerRecord.race and 1 or 0
+
+        return operator.check(val, ref.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerGender] = function (req)
+        if not req.value then return end
+
+        local playerRecord = types.NPC.record(playerRef)
+        local val = playerRecord.isMale and 0 or 1
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerLevel] = function (req)
+        if not req.value then return end
+        return operator.check(types.Actor.stats.level(playerRef).current, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.Weather] = function (req, ref)
+        if not req.value or not tes3 then return end
+
+        local weatherId = tes3.weather[core.weather.getCurrent((ref or playerRef).cell).recordId]
+        if not weatherId then return end
+
+        return operator.check(weatherId, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCReputation] = function (req, ref)
+        if not req.value or not req.object or not req then return end
+
+        if ref.recordId ~= req.object then return false end
+        local val = types.NPC.getDisposition(ref, playerRef)
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerHealth] = function (req)
+        if not req.value then return end
+
+        local health = types.Actor.stats.dynamic.health(playerRef)
+
+        return operator.check(health.current, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCFlee] = function (req, ref)
+        if not req.value or not ref then return end
+
+        if req.object and ref.recordId ~= req.object then return false end
+        local val = types.Actor.stats.ai.flee(req)
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCHello] = function (req, ref)
+        if not req.value or not ref then return end
+
+        if req.object and ref.recordId ~= req.object then return false end
+        local val = types.Actor.stats.ai.hello(req)
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCAlarm] = function (req, ref)
+        if not req.value or not ref then return end
+
+        if req.object and ref.recordId ~= req.object then return false end
+        local val = types.Actor.stats.ai.alarm(req)
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCFight] = function (req, ref)
+        if not req.value or not ref then return end
+
+        if req.object and ref.recordId ~= req.object then return false end
+        local val = types.Actor.stats.ai.fight(req)
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCLevel] = function (req, ref)
+        if not req.value or not ref then return end
+        if req.object and ref.recordId ~= req.object then return false end
+
+        return operator.check(types.Actor.stats.level(ref).current, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NPCIsWerewolf] = function (req, ref)
+        if not req.value or not ref then return end
+         if req.object == "player" then
+            ref = playerRef
+        end
+        if req.object and ref.recordId ~= req.object then return false end
+
+        local val = types.NPC.isWerewolf(ref) and 1 or 0
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.NotActorFaction] = function (req, ref)
+        if not req.value or not req.variable or not ref then return end
+        local factionRank = types.NPC.getFactionRank(ref, req.variable)
+
+        local val = factionRank == 0 and 1 or 0
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerCommonDisease] = function (req)
+        if not req.value then return end
+
+        local val = 0
+        for _, spell in pairs(types.Actor.spells(playerRef)) do
+            val = spell.type == core.magic.SPELL_TYPE.Disease and 1 or 0
+            if val == 1 then break end
+        end
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerBlightDisease] = function (req)
+        if not req.value then return end
+
+        local val = 0
+        for _, spell in pairs(types.Actor.spells(playerRef)) do
+            val = spell.type == core.magic.SPELL_TYPE.Blight and 1 or 0
+            if val == 1 then break end
+        end
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
+    [reqTypes.requirementType.PlayerCorprus] = function (req)
+        if not req.value then return end
+
+        local eff = types.Actor.activeEffects(playerRef):getEffect(core.magic.EFFECT_TYPE.Corprus)
+        local val = eff.magnitude > 0 and 1 or 0
+
+        return not operator.check(val, req.value, req.operator)
+    end,
+
+
     -- [reqTypes.requirementType.CustomActor] = function (req, ref)
     --     if not req.object or not req.variable or not req.value or not ref then return end
     --     local dialogue = tes3.findDialogue{ topic = stringLib.convertDialogueName(req.variable) }
@@ -143,6 +542,16 @@ local dataFuncs = {
     --     return dialogueInfo:filter(ref.object, ref, 0, dialogue)
     -- end,
 
+    [reqTypes.requirementType.CustomDisposition] = function (req, ref)
+        if not req.value or not req then return end
+
+        if req.object and ref.recordId ~= req.object then return false end
+
+        local val = types.NPC.getDisposition(ref, playerRef)
+
+        return operator.check(val, req.value, req.operator)
+    end,
+
     [reqTypes.requirementType.CustomDialogue] = function (req)
         if not req.variable then return end
         local dialogueId = stringLib.convertDialogueName(req.variable)
@@ -152,6 +561,8 @@ local dataFuncs = {
         return operator.check(false, true, req.operator)
     end,
 }
+
+this.dataFuncs = dataFuncs
 
 
 ---@param req questDataGenerator.requirementData
@@ -172,16 +583,31 @@ end
 ---@field reference tes3reference?
 ---@field ignoredTypes table<string, any>?
 ---@field allowedTypes table<string, any>?
+---@field typeTruthTable table<string, boolean>?
 ---@field threatErrorsAs boolean?
 
 ---@param block questDataGenerator.requirementData[]
 ---@param params questGuider.requirementChecker.checkForBlock.params
 ---@return boolean?
+---@return questDataGenerator.requirementData[]? ignoredRequirements
 function this.checkBlock(block, params)
+    if not params then params = {} end
+    if not params.ignoredTypes then params.ignoredTypes = {} end
+
+    -- don't forget to remove when this requirement type will be supported
+    params.ignoredTypes[reqTypes.requirementType.CustomActor] = true
+
+    local ignoredRequirements = {}
     local res = true
     for _, req in pairs(block) do
         if (params.ignoredTypes and params.ignoredTypes[req.type]) or
                 (params.allowedTypes and not params.allowedTypes[req.type]) then
+            table.insert(ignoredRequirements, req)
+            goto continue
+        end
+
+        if params.typeTruthTable and params.typeTruthTable[req.type] then
+            res = res and params.typeTruthTable[req.type]
             goto continue
         end
 
@@ -204,7 +630,7 @@ function this.checkBlock(block, params)
         ::continue::
     end
 
-    return res
+    return res, ignoredRequirements
 end
 
 
