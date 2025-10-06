cf0a871d854d7ebb7bd0dec55015a08572d58391
Diject <alexkozlovv@gmail.com>
2025-10-06 20:23:04 +0300

    fix nil reference in cell region name formatting

--- scripts/quest_guider_lite/core/cellData.lua
+++ scripts/quest_guider_lite/core/cellData.lua
@@ -12,7 +12,8 @@ function this.getCellData(cell)
 		gridY = cell.isExterior and cell.gridY or nil,
 		id = not cell.isExterior and cell.id or nil,
 		name = cell.isExterior and string.format("%s (%d, %d)",
-			core.regions.records[cell.region or ""] and core.regions.records[cell.region or ""].name or cell.region or "", cell.gridX, cell.gridY) or cell.name,
+			core.regions and core.regions.records[cell.region or ""] and core.regions.records[cell.region or ""].name
+			or cell.region or "", cell.gridX, cell.gridY) or cell.name,
 	}
 	return dt
 end
